a5d4aed083f20738f775ae6dd3a805e5728de67a
JP Caparas <jp@jpcaparas.com>
2024-02-29 01:45:41 +1300

    Allow package discovery

---
+++ src/Providers/SqlDumpValidatorServiceProvider.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Zenoware\Laravel\SqlDumpValidator\Providers;
+
+use Illuminate\Support\ServiceProvider as ServiceProviderBase;
+use Zenoware\Laravel\SqlDumpValidator\Adapters\IFileAdapter;
+use Zenoware\Laravel\SqlDumpValidator\Adapters\LocalFileAdapter;
+use Zenoware\Laravel\SqlDumpValidator\Services\SqlDumpValidatorService;
+use Zenoware\Laravel\SqlDumpValidator\Console\ValidateSqlDumpCommand;
+
+class SqlDumpValidatorServiceProvider extends ServiceProviderBase
+{
+    public function register()
+    {
+        $this->app->singleton(IFileAdapter::class, LocalFileAdapter::class);
+        $this->app->singleton(SqlDumpValidatorService::class, function ($app) {
+            return new SqlDumpValidatorService($app->make(IFileAdapter::class));
+        });
+    }
+
+    public function boot()
+    {
+        if ($this->app->runningInConsole()) {
+            $this->commands([
+                ValidateSqlDumpCommand::class,
+            ]);
+        }
+    }
+}
